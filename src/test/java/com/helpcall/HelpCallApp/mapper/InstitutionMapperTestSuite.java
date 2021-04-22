435cb7c37e69f01ed99be1e0571cbca7bef50954
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2021-04-22 11:33:17 +0200

    InstitutionMapper tests - fixed bug

--- src/test/java/com/helpcall/HelpCallApp/mapper/InstitutionMapperTestSuite.java
+++ src/test/java/com/helpcall/HelpCallApp/mapper/InstitutionMapperTestSuite.java
@@ -21,13 +21,19 @@ public class InstitutionMapperTestSuite {
     @Test
     void testInstitutionMapper() {
         //given
-        Institution institution = new Institution(1L, "test", "test@test");
-        Institution institution1 = new Institution(2L, "test2", "test2@test2");
-        Institution institution2 = new Institution(3L, "test3", "test3@test3");
+        Institution institution = new Institution(1L, "test", "test@test", "test", "lat",
+                "lon", "description","isInstitution", new ArrayList<>());
+        Institution institution1 = new Institution(2L, "test2", "test2@test2", "test", "lat",
+                "lon", "description","isInstitution", new ArrayList<>());
+        Institution institution2 = new Institution(3L, "test3", "test3@test3", "test", "lat",
+                "lon", "description","isInstitution", new ArrayList<>());
 
-        InstitutionDto institutionDto = new InstitutionDto(1L, "test2", "test2@test2");
-        InstitutionDto institutionDto1 = new InstitutionDto(2L, "test2", "test2@test2");
-        InstitutionDto institutionDto2 = new InstitutionDto(3L, "test3", "test3@test3");
+        InstitutionDto institutionDto = new InstitutionDto(1L, "test2", "test2@test2", "test", "lat",
+                "lon", "description","isInstitution", new ArrayList<>());
+        InstitutionDto institutionDto1 = new InstitutionDto(2L, "test2", "test2@test2", "test", "lat",
+                "lon", "description","isInstitution", new ArrayList<>());
+        InstitutionDto institutionDto2 = new InstitutionDto(3L, "test3", "test3@test3", "test", "lat",
+                "lon", "description","isInstitution", new ArrayList<>());
 
         List<Institution> institutions = new ArrayList<>();
         List<Institution> institutions1;
